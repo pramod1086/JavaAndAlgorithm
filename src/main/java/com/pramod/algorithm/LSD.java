1473e4a0bc354214f944ff86d3c85921d5cb3f92
Pramod Kumar Singh <71481969+ps185370@users.noreply.github.com>
2023-03-30 09:26:56 +0530

    algorithm

---
+++ src/main/java/com/pramod/algorithm/LSD.java
@@ -0,0 +1,33 @@
+package com.pramod.algorithm;
+
+public class LSD {
+    public static void main(String []args){
+
+        String [] str = {"4PGC938","2IYE230","3CIO720","1ICK750","1OHV845"};
+        sort(str,str.length-1);
+        for(String str1:str){
+            System.out.println(str1);
+        }
+
+    }
+    public static void sort(String[] a, int W)
+    {  // Sort a[] on leading W characters.
+        int N = a.length;
+        int R = 256;
+        String[] aux = new String[N];
+        for (int d = W-1; d >= 0; d--)
+        { // Sort by key-indexed counting on dth char.
+            //  String [] str = {"4PGC938","2IYE230","3CIO720","1ICK750","1OHV845"};
+            int[] count = new int[R+1];     // Compute frequency counts.
+            for (int i = 0; i < N; i++)
+                count[a[i].charAt(d) + 1]++;
+            for (int r = 0; r < R; r++)     // Transform counts to indices.
+                count[r+1] += count[r];
+            for (int i = 0; i < N; i++)     // Distribute.
+                aux[count[a[i].charAt(d)]++] = a[i];
+            for (int i = 0; i < N; i++)     // Copy back.
+                a[i] = aux[i];
+        }
+    }
+
+}
