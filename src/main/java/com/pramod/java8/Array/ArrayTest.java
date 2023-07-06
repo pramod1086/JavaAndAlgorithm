b4fd29bdc101f4e0edf70df157d6807a2cebc5f2
ps185370 <ps185370@ncr.com>
2023-07-06 18:00:12 +0530

    Array initialization in java

---
+++ src/main/java/com/pramod/java8/Array/ArrayTest.java
@@ -0,0 +1,19 @@
+package com.pramod.java8.Array;
+
+import java.util.Arrays;
+
+public class ArrayTest {
+    public static void main(String args[]){
+
+        /// One way of array initialization
+        int [] arr = {1,2,3,4,5};
+        // another way of array initialization
+        int[] num = new int[]{1,2,3,4,5};
+        /// increment of array in java.Each value is incremented
+        for(int i = 0; i < arr.length; i++)
+        {
+            arr[i]++;
+        }
+        Arrays.stream(arr).forEach(l->System.out.println(l));
+    }
+}
