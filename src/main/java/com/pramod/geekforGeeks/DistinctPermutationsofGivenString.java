8f266e36c8f11c65e271dcf7bbad14ba50e5c21e
Pramod Kumar Singh <ps185370@ncr.com>
2024-02-13 12:33:54 +0530

    LinkedIn Optimization

---
+++ src/main/java/com/pramod/geekforGeeks/DistinctPermutationsofGivenString.java
@@ -0,0 +1,39 @@
+package com.pramod.geekforGeeks;
+
+public class DistinctPermutationsofGivenString {  public static void main(String args[]){
+    DistinctPermutationsofGivenString ls = new DistinctPermutationsofGivenString();
+    String str = "ABC";
+    ls.distinctPermutationsofGivenString(str.toCharArray(),0,str.length()-1);
+
+}
+
+    public char [] swap(char charArray[], int i, int j)
+    {
+        char temp;
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return charArray;
+    }
+    public boolean shouldSwap(char str[], int start, int curr) {
+        for (int i = start; i < curr; i++) {
+            if (str[i] == str[curr]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private void distinctPermutationsofGivenString(char str[],int left,int right) {
+        if (left == right)
+            System.out.println(str);
+        else {
+            for (int i = left; i <= right; i++) {
+                boolean check = shouldSwap(str, left, i);
+                str = swap(str, left, i);
+                distinctPermutationsofGivenString(str, left + 1, right);
+                str = swap(str, left, i);
+            }
+        }
+    }
+
+}
