21da92e5919b91ddede187e8bf495396259d9384
Pramod Kumar Singh <ps185370@ncr.com>
2024-02-22 10:59:20 +0530

    LinkedIn Optimization

--- src/main/java/com/pramod/algorithm/leetcode/FindKtheElementUsingPriorityQueue.java
+++ src/main/java/com/pramod/algorithm/leetcode/FindKtheElementUsingPriorityQueue.java
@@ -1,30 +1,22 @@
 package com.pramod.algorithm.leetcode;
 
-import java.util.PriorityQueue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class FindKtheElementUsingPriorityQueue {
     public static void main(String args[]) {
-        Integer arr[] = { 3,2,3,1,2,4,5,5,6};
+        int arr[] = { 3,2,3,1,2,4,5,5,6};
         int k = findKtheElementUsingPriorityQueue(arr,  4);
         System.out.print(k);
     }
 
-    private static int findKtheElementUsingPriorityQueue(Integer[] nums, int k) {
+    private static int findKtheElementUsingPriorityQueue(int[] nums, int k) {
         int ans = Integer.MAX_VALUE;
-        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
-        for(int i=0;i<nums.length;i++){
-
-            minHeap.add(nums[i]);
-            if(minHeap.size() > k){
-                minHeap.remove();
-            }
-
-        }
-        while(minHeap.size() != 0){
-            if(minHeap.peek() < ans){
-                ans = minHeap.peek();
-            }
-            minHeap.remove();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Comparator.reverseOrder());
+        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        maxHeap.addAll(list);
+        for(int i =0;i<k;i++){
+            ans = maxHeap.poll();
         }
         return ans;
     }
